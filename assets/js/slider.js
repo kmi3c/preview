b2171a010cec25a2e7db7d01c6d5d98c192dbf72
kmi3c <pawel.adamski85@wp.pl>
2017-09-02 22:35:20 +0200

    WIP: Add ajax image loading scaffold ;)

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,4 +1,15 @@
 //Slider
 $(function() {
-  alert('ready!');
+  var slider = $('#slider');
+  var gallery;
+    $.get('/i',function(data, status){
+      gallery = data;
+      $.each(gallery.albums, function(index, value){
+        slider.append('<section id="album-'+index+'">'+value.name+'</section>');
+        $.each(value.images, function(i,image){
+            console.log(image);
+          $('#album-' + index).append('<img src="/i/'+ image.url +'"/>');
+        });
+    });
+  });
 });
