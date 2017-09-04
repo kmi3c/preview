a19135a7fc663089f221f472909545100af1b545
kmi3c <pawel.adamski85@wp.pl>
2017-09-04 21:33:12 +0200

    Preload more images while viewing.

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,8 @@
 //Slider
 var Slider = {
-  init(selector){
+  init(selector, options = {}){
+    // TODO: merge options
+    this.options = { preload: 2 };
     this.elem = $(selector);
     if(this.elem){
       this.load_data();
@@ -16,9 +18,7 @@ var Slider = {
       $.each(this.gallery.albums, function(index, value){
         slider.elem.append('<section class="album" id="album-'+index+'"><a href="#'+value.name+'">'+value.name+'</a></section>');
         $.each(value.files, function(i,file){
-          if(file.type == 'image'){
-            $('#album-' + index).append('<img class="file" data-src="/i/'+ file.url +'"/>');
-          }
+          slider.draw_file(index,file);
         });
       });
     });
@@ -44,6 +44,7 @@ var Slider = {
     });
     this.elem.on('click', '.album', function() {
       slider.set_album($(this));
+      slider.next_file(1);
     });
   },
   next_file(increment){
@@ -53,9 +54,19 @@ var Slider = {
     }
     this.current_album.items.hide();
     var item = $(this.current_album.items[this.current_album.index])
-    // TODO: preload more images.
+    for(i = 0; i < this.options.preload; i++){
+      preload  = $(this.current_album.items[this.current_album.index + increment])
+      if(!preload.attr('src')){
+        preload.attr('src', preload.data('src'));
+      }
+    }
     item.attr('src',item.data('src'));
     item.css('display','inline-block')
+  },
+  draw_file(index, file){
+    if(file.type == 'image'){
+      $('#album-' + index).append('<img class="file" data-src="/i/'+ file.url +'"/>');
+    }
   }
 };
 $(function() {
